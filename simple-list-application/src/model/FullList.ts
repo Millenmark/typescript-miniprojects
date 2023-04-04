a2d52dc2d39d1a7576bae40dde152a7c876f9048
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-04-04 19:49:02 +0800

    Created FullList.ts
    Added interface and imports ListItem

---
+++ simple-list-application/src/model/FullList.ts
@@ -0,0 +1,10 @@
+import ListItem from "./ListItem"
+
+interface List {
+  list: ListItem[],
+  load(): void,
+  save(): void,
+  clearList(): void,
+  addItem(ItemObj: ListItem): void,
+  removeItem(id: string): void,
+}
